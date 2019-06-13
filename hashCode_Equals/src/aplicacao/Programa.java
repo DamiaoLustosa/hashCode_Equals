729a0feee055dbc269bd55b9ea2ebf33c4430bf9
DamiaoLustosa <50031115+DamiaoLustosa@users.noreply.github.com>
2019-06-13 16:17:05 -0300

    Usando hascode e equals de maneira simples
    Usando hascode e equals de maneira simples - sem personalização

---
+++ hashCode_Equals/src/aplicacao/Programa.java
@@ -0,0 +1,27 @@
+package aplicacao;
+
+
+public class Programa {
+	public static void main (String args[]) {
+		
+		String a = "Maria";
+		String b = "Alex";
+		
+		//equals compara se um objeto é igual ao outro, retornando true ou false
+		System.out.println("Método equals: " + a.equals(b)); 
+		
+		//hashCode retorna um número inteiro representando um código gerado a partir das informações do objeto
+		String c = "Antonio";
+		String d = "Francisco";
+		System.out.println("hashCode C: " +c.hashCode());
+		System.out.println("hashCode D: " + d.hashCode());
+		
+		
+		
+	
+		
+		
+		
+	}
+	
+}

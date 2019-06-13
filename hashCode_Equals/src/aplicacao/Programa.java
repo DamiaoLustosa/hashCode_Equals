475c1cc4be29e01bc5ce0dc631e46cf7122ed92a
DamiaoLustosa <50031115+DamiaoLustosa@users.noreply.github.com>
2019-06-13 16:27:42 -0300

    Comparando valores com ==
    o == compara os objetos por referência de memória e não por conteúdo, como faz o equals

--- hashCode_Equals/src/aplicacao/Programa.java
+++ hashCode_Equals/src/aplicacao/Programa.java
@@ -6,12 +6,12 @@ public class Programa {
 	public static void main (String args[]) {
 		
 	Cliente c1 = new Cliente("Maria", "maria@gmail.com");
-	Cliente c2 = new Cliente("Maria", "antony@gmail.com");
+	Cliente c2 = new Cliente("Maria", "maria@gmail.com");
 	
 	System.out.println(c1.hashCode());
 	System.out.println(c2.hashCode());
 	System.out.println(c1.equals(c2));
-		
+	System.out.println(c1==c2);	//Retorna falso porque o == compara as referências de memórias
 		
 	}
 	
